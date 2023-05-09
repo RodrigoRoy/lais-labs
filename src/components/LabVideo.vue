0d53eb9d8602cc6c9aabfcd51f5ecb664fa42783
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2023-05-09 15:32:17 -0600

    habilitar reproducción de video

--- src/components/LabVideo.vue
+++ src/components/LabVideo.vue
@@ -5,7 +5,7 @@
     <!-- No se usan v-card-title/subtitle para evitar wrap a una línea, impidiendo leer el nombre y ubicación del laboratorio -->
     <v-card-text class="pb-0">
       <!-- VIDEO -->
-      <iframe v-if="laboratorio.video" width="100%" height="350" class="mt-0" :src="laboratorio.video" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe v-show="laboratorio.video" width="100%" height="350" class="mt-0" :src="laboratorio.video" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="position: relative; z-index: 1;"></iframe>
     </v-card-text>
   </v-card>
 </template>
@@ -18,21 +18,9 @@ export default {
     // Información completa del laboratorio (@see src/data/labs.mjs)
     laboratorio: { type: Object }, 
   },
-
-  data: () => ({
-    // Ruta del directorio /public
-    publicPath: process.env.BASE_URL,
-  }),
+  
 };
 </script>
-
-<style scoped>
-iframe[src*=youtube]
-{
-  max-width:100%;
-  max-height:100%;
-}
-</style>
 
 <style>
 /* Usar en conjunto con background-text (aparentemente no es necesario) */
